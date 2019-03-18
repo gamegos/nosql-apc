e7a8e0cf7c4046f3be97e7b4c0b6c7c091e1eb94
Safak Ozpinar <safak@gamegos.com>
2019-03-18 16:37:55 +0300

    Improve tests for unexpected APC behaviours

--- tests/Storage/ApcTest.php
+++ tests/Storage/ApcTest.php
@@ -1,9 +1,12 @@
 <?php
 namespace Gamegos\NoSql\Tests\Storage;
 
-/* Import from gamegos/nosql */
+/* Imports from gamegos/nosql */
 use Gamegos\NoSql\Storage\Apc;
 use Gamegos\NoSql\Storage\Exception\ApcExtensionException;
+
+/* Imports from PHP core */
+use UnexpectedValueException;
 
 /**
  * Test Class for Storage\Apc
@@ -51,6 +54,9 @@ class ApcTest extends AbstractCommonStorageTest
             }
             if ($this->getName() == 'testApcDisabled' || $this->getName() == 'testApcCliDisabled') {
                 uopz_unset_return('ini_get');
+            }
+            if ($this->getName() == 'testIncrementShouldThrowExceptionOnExistingIntegerFail') {
+                uopz_unset_return('apcu_inc');
             }
         }
         apcu_clear_cache();
@@ -140,6 +146,123 @@ class ApcTest extends AbstractCommonStorageTest
     {
         $apc = $this->createStorage();
         $this->assertEquals(phpversion('apcu'), $apc->getApcuVersion());
+    }
+
+    public function testAddShouldThrowExceptionForResource()
+    {
+        if (version_compare(phpversion('apcu'), '5.1.12', '>')) {
+            $apc   = $this->createStorage();
+            $key   = 'foo';
+            $value = fopen('php://memory', 'r');
+            $this->setExpectedException(
+                UnexpectedValueException::class,
+                'APCu version 5.1.13 and above cannot store resources.'
+            );
+            $apc->add($key, $value);
+        } else {
+            $this->markTestSkipped('Test is only for APCu version 5.1.13 and above.');
+        }
+    }
+
+    public function testSetShouldThrowExceptionForResource()
+    {
+        if (version_compare(phpversion('apcu'), '5.1.12', '>')) {
+            $apc   = $this->createStorage();
+            $key   = 'foo';
+            $value = fopen('php://memory', 'r');
+            $this->setExpectedException(
+                UnexpectedValueException::class,
+                'APCu version 5.1.13 and above cannot store resources.'
+            );
+            $apc->set($key, $value);
+        } else {
+            $this->markTestSkipped('Test is only for APCu version 5.1.13 and above.');
+        }
+    }
+
+    public function testCasShouldThrowExceptionForResource()
+    {
+        if (version_compare(phpversion('apcu'), '5.1.12', '>')) {
+            $apc   = $this->createStorage();
+            $key   = 'foo';
+            $value = fopen('php://memory', 'r');
+            $token = null;
+            $this->setExpectedException(
+                UnexpectedValueException::class,
+                'APCu version 5.1.13 and above cannot store resources.'
+            );
+            $apc->cas($token, $key, $value);
+        } else {
+            $this->markTestSkipped('Test is only for APCu version 5.1.13 and above.');
+        }
+    }
+
+    public function testIncrementShouldReturnFalseOnInitialFail()
+    {
+        if (function_exists('uopz_set_return') && function_exists('uopz_unset_return')) {
+            $apc = new Apc();
+            uopz_set_return('apcu_store', false);
+            $this->assertFalse($apc->increment('foo'));
+            uopz_unset_return('apcu_store');
+        } else {
+            $this->markTestSkipped('The uopz extension is not available.');
+        }
+    }
+
+    public function testIncrementShouldThrowExceptionOnExistingIntegerFail()
+    {
+        if (function_exists('uopz_set_return') && function_exists('uopz_unset_return')) {
+            $val = 100;
+            $apc = new Apc();
+            $apc->add('foo', $val);
+            // Integer value will not be increased.
+            uopz_set_return(
+                'apcu_inc',
+                function ($key, $step = 1, &$success = false) {
+                    $success = false;
+                    return false;
+                },
+                true
+            );
+            $this->setExpectedException(
+                UnexpectedValueException::class,
+                sprintf('APC could not increment integer value (%d).', $val)
+            );
+            $apc->increment('foo');
+        } else {
+            $this->markTestSkipped('The uopz extension is not available.');
+        }
+    }
+
+    public function testIncrementShouldReturnFalseOnExistingValueFail()
+    {
+        if (function_exists('uopz_set_return') && function_exists('uopz_unset_return')) {
+            $apc = new Apc();
+            $apc->add('foo', 100);
+            // Existing value will not be increased.
+            uopz_set_return(
+                'apcu_inc',
+                function ($key, $step = 1, &$success = false) {
+                    $success = false;
+                    return false;
+                },
+                true
+            );
+            // Then existing value will not be fetched.
+            uopz_set_return(
+                'apcu_fetch',
+                function ($key, &$success = false) {
+                    $success = false;
+                    return false;
+                },
+                true
+            );
+            $this->assertFalse($apc->increment('foo'));
+            uopz_unset_return('apcu_inc');
+            uopz_unset_return('apcu_fetch');
+        } else {
+            $this->markTestSkipped('The uopz extension is not available.');
+        }
     }
 
     /**
